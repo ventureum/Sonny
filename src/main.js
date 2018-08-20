3d96823bcf420a609006f8242466eb378ebd694a
Timothy Wang <timothywangdev@gmail.com>
2018-08-20 18:44:15 +0000

    Resolve #1 Lambda ready

--- src/main.js
+++ src/main.js
@@ -1,22 +1,17 @@
 import 'babel-polyfill'
-import telegrafAws from 'telegraf-aws'
 const Telegraf = require('telegraf')
 const Markup = require('telegraf/markup')
 
 const bot = new Telegraf(process.env.BOT_TOKEN, {
   // set to false for local testing
-  webhookReply: process.env.BOT_LOCAL
+  webhookReply: process.env.BOT_MODE === 'WEBHOOK'
 })
 
-if (process.env.BOT_LOCAL) {
+if (process.env.BOT_LOCAL === 'true') {
   console.log('=============== LOCAL TESTING MODE ===============')
 } else {
-  bot.telegram.setWebhook(process.env.BOT_WEBHOOK_URL)
+  // bot.telegram.setWebhook(process.env.BOT_WEBHOOK_URL)
 }
-
-const updateHandler = telegrafAws(bot, {
-  timeout: 1000 // Optional parameter, after timeout, empty response will be sent to AWS and function execution will be stopped
-})
 
 var voteCounter = {}
 
@@ -77,18 +72,22 @@ bot.action('downvote', async (ctx) => {
 })
 
 bot.command('p', (ctx) => {
-  console.log(ctx)
   let messageId = ctx.message.message_id
   voteCounter[messageId] = {upvote: 0, downvote: 0}
   ctx.telegram.sendMessage(ctx.chat.id, '!', {reply_to_message_id: ctx.message.message_id, reply_markup: keyboard, disable_notification: true})
 })
 
 exports.handler = async (event, context, callback) => {
-  updateHandler(event, callback)
+  const body = event.body // get data passed to us
+  bot.handleUpdate(body) // make Telegraf process that data
+  return callback(null, { // return something for webhook, so it doesn't try to send same stuff again
+    statusCode: 200,
+    body: ''
+  })
 }
 
 // local testing
-if (process.env.BOT_LOCAL) {
+if (process.env.BOT_MODE === 'LOCAL') {
   console.log('=============== LOCAL POLLING ===============')
-  //bot.startPolling()
+  bot.startPolling()
 }
